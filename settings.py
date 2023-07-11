112bfa2e98cc4c93c4304b08bd3e87adfb9f5183
Hisyam Musrin <137146174+SyamMusrin@users.noreply.github.com>
2023-07-11 14:31:25 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,10 +2,12 @@
 
 #=========================================
 sr = 22500 #(Sample rate; Hz)
-d = 3 #(Duration; seconds)
+d = 3.0 #(Duration; seconds)
 bs = 1024 #(Buffer size)
 t = -38 #(threshold;dB)
 bd = 16 #(Bit Depth)
+ch = 2 #(Channels)
+file_format = '.wav'
 #=========================================
 
 #AWS Connection Settings
@@ -13,4 +15,13 @@ bd = 16 #(Bit Depth)
 ACCESS_KEY = ''
 SECRET_KEY = ''
 BUCKET = 'awsgwsbk-1'
+folder_dir = 'audio/recorded_audio/gws/'
+#=========================================
+
+#MySQL Connection Settings
+#=========================================
+rds_host = ''
+rds_user = ''
+drs_pass = ''
+rds_db_name = 'gws'
 #=========================================
