81e3fa7fe0a8736207f7f3c6a84124f415565dd1
Hisyam Musrin <137146174+SyamMusrin@users.noreply.github.com>
2023-07-11 14:29:14 +0800

    Update logging.py

--- modules/logging.py
+++ modules/logging.py
@@ -1,6 +1,10 @@
 #Logging Function
 import csv
 import os
+import mysql.connector
+
+conn = pymysql.connect(host=rds_host, user=rds_user,
+ password=rds_pass, database=rds_db_name)
 
 class Logging():
 	#Checks for last entry
@@ -14,7 +18,7 @@ class Logging():
 	#Creates a new .csv file
 	def Create():
 		with open("logs.csv", mode ="w") as csvfile:
-			fieldnames = ["no.", "date", "time"]
+			fieldnames = ["no.", "file_format", "sample_rate", "bit_depth", "duration", "date", "time"]
 			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 			writer.writeheader()
 
@@ -26,18 +30,20 @@ class Logging():
 		file.close()	
 
 	#logging function
-	def New_Entry(date, time):
-
+	def New_Entry(file_format, sr, bd, d, date, time):
 		#Creates a new entry if logs.csv exist if not create file and input new entry.
-
 		if os.path.isfile('logs.csv'):
 			num = int(Logging.Last()) + 1
-			add_data = [[num, date, time]]
-
+			add_data = [[num, file_format, sr, bd, d, date, time]]
 			#Create a new entry for the .csv file
 			Logging.Add(add_data)
 		
 		else:
 			print("logs.csv does not exist. Creating .csv file now...")
 			Logging.Create()
-			Logging.Add([[1, date, time]])
+
+			num = 1
+			add_data = [[num, file_format, sr, bd, date, time]]
+			Logging.Add(add_data)
+
+
