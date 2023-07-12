04ca357bee5835e8b82c6e28f86643fa8a98d8bc
Hisyam Musrin <137146174+SyamMusrin@users.noreply.github.com>
2023-07-12 11:43:53 +0800

    Update logging.py

--- modules/logging.py
+++ modules/logging.py
@@ -1,49 +1,26 @@
 #Logging Function
-import csv
-import os
-import mysql.connector
+import pymysql
 
-conn = pymysql.connect(host=rds_host, user=rds_user,
+from settings import *
+
+
+connection = pymysql.connect(host=rds_host, user=rds_user,
  password=rds_pass, database=rds_db_name)
 
 class Logging():
 	#Checks for last entry
 	def Last():
-		with open('logs.csv','r') as file: 
-			data = file.readlines() 
-			last_row = data[-1].rstrip('\n')
-			first_column_data = last_row.split(',')[0] 
-			return first_column_data
+		# Create a cursor
+		cursor = connection.cursor()
+		# Execute SELECT query
+		query = "SELECT audioname FROM audio_samples WHERE dataset_id = 1 ORDER BY sample_id DESC LIMIT 1"
 
-	#Creates a new .csv file
-	def Create():
-		with open("logs.csv", mode ="w") as csvfile:
-			fieldnames = ["no.", "file_format", "sample_rate", "bit_depth", "duration", "date", "time"]
-			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-			writer.writeheader()
+		cursor.execute(query)
+		# Fetch the result
+		result = cursor.fetchone()
 
-	#Adds a new line to the .csv file
-	def Add(add_data):
-		file = open("logs.csv", mode = "a", newline = "")
-		writer = csv.writer(file)
-		writer.writerows(add_data)
-		file.close()	
+		# Extract the number
+		last_number = ''.join(filter(str.isdigit, result[0]))
 
-	#logging function
-	def New_Entry(file_format, sr, bd, d, date, time):
-		#Creates a new entry if logs.csv exist if not create file and input new entry.
-		if os.path.isfile('logs.csv'):
-			num = int(Logging.Last()) + 1
-			add_data = [[num, file_format, sr, bd, d, date, time]]
-			#Create a new entry for the .csv file
-			Logging.Add(add_data)
-		
-		else:
-			print("logs.csv does not exist. Creating .csv file now...")
-			Logging.Create()
-
-			num = 1
-			add_data = [[num, file_format, sr, bd, date, time]]
-			Logging.Add(add_data)
-
-
+		number = int(last_number) + 1
+		return number
